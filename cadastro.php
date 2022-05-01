a8a52f4bbb1760bfd3a6ee93e86aaecdb39c2573
JV-Repo <joao.assumpo@gmail.com>
2022-04-30 23:59:17 -0300

    Correções

--- cadastro.php
+++ cadastro.php
@@ -1,64 +1,76 @@
-<?php
+<html>
+  <?php require 'cadastro.html';?>
+  <?php
 
-$login = $_POST['login'];
-$senha = MD5($_POST['senha']);
-$nome = $_POST['nome'];
-$cpf = $_POST['cpf'];
-$telefone = $_POST['telefone'];
-$connect = mysql_connect('nome_do_servidor','nome_de_usuario','senha');
-$db = mysql_select_db('nome_do_banco_de_dados');
-$query_select = "SELECT login FROM usuario WHERE login = '$login'";
-$select = mysql_query($query_select,$connect);
-$array = mysql_fetch_array($select);
-$logarray = $array['login'];
+  $login    = $_POST['login'];
+  $senha    = MD5($_POST['senha']);
+  $nome     = $_POST['nome'];
+  $cpf      = $_POST['cpf'];
+  $telefone = $_POST['telefone'];
 
-  
-if($login == "" || $login == null){
-    echo"<script language='javascript' type='text/javascript'>
-    alert('O campo login deve ser preenchido');window.location.href='
-    cadastro.html';</script>";
-  
-if($senha == "" || $senha == null){
-echo"<script language='javascript' type='text/javascript'>
-alert('O campo senha deve ser preenchido');window.location.href='
-cadastro.html';</script>";
+  $connect = mysqli_connect('localhost','root','','EcoStore');
+  $query_select = "SELECT login FROM usuario WHERE login = '$login'";
+  $select = mysqli_query($query_select,$connect);
+  $array = mysqli_fetch_array($select);
+  /*$logarray = $array['login'];*/
 
-if($nome == "" || $nome == null){
-echo"<script language='javascript' type='text/javascript'>
-alert('O campo nome deve ser preenchido');window.location.href='
-cadastro.html';</script>";
-
-if($cpf == "" || $cpf == null){
-echo"<script language='javascript' type='text/javascript'>
-alert('O campo cpf deve ser preenchido');window.location.href='
-cadastro.html';</script>";
     
-if($telefone == "" || $telefone == null){
+  $insert = "INSERT INTO Usuario (login,senha,nome,cpf,telefone) VALUES ('$login','$senha','$nome','$cpf','$telefone')";
+  if ($res = mysqli_query($connect,$insert)){
+    echo"<script language='javascript' type='text/javascript'>
+            alert('Usuário cadastrado com sucesso!');window.location.
+            href='login.html'</script>";
+  }else{
+    echo "Erro: " . mysqli_error($connect);
+  }
+
+  /*if($login == "" || $login == null){
       echo"<script language='javascript' type='text/javascript'>
-      alert('O campo telefone deve ser preenchido');window.location.href='
-      cadastro.html';</script>";
+      alert('O campo login deve ser preenchido');window.location.href='
+      cadastro.html';</script>";}
+    
+  if($senha == "" || $senha == null){
+  echo"<script language='javascript' type='text/javascript'>
+  alert('O campo senha deve ser preenchido');window.location.href='
+  cadastro.html';</script>";}
 
-    }else{
-      if($logarray == $login){
+  if($nome == "" || $nome == null){
+  echo"<script language='javascript' type='text/javascript'>
+  alert('O campo nome deve ser preenchido');window.location.href='
+  cadastro.html';</script>";}
 
+  if($cpf == "" || $cpf == null){
+  echo"<script language='javascript' type='text/javascript'>
+  alert('O campo cpf deve ser preenchido');window.location.href='
+  cadastro.html';</script>";}
+      
+  if($telefone == "" || $telefone == null){
         echo"<script language='javascript' type='text/javascript'>
-        alert('Esse login já existe');window.location.href='
+        alert('O campo telefone deve ser preenchido');window.location.href='
         cadastro.html';</script>";
-        die();
 
       }else{
-        $query = "INSERT INTO usuario (login,senha,nome,,cpf,telefone) VALUES ('$login','$senha','$nome','$cpf','$telefone')";
-        $insert = mysql_query($query,$connect);
+        if($logarray == $login){
 
-        if($insert){
           echo"<script language='javascript' type='text/javascript'>
-          alert('Usuário cadastrado com sucesso!');window.location.
-          href='login.html'</script>";
+          alert('Esse login já existe');window.location.href='
+          cadastro.html';</script>";
+          die();
+
         }else{
-          echo"<script language='javascript' type='text/javascript'>
-          alert('Não foi possível cadastrar esse usuário');window.location
-          .href='cadastro.html'</script>";
+          $query = "INSERT INTO usuario (login,senha,nome,cpf,telefone) VALUES ('$login','$senha','$nome','$cpf','$telefone')";
+          $insert = mysqli_query($connect,$query);
+
+          if($insert){
+            echo"<script language='javascript' type='text/javascript'>
+            alert('Usuário cadastrado com sucesso!');window.location.
+            href='login.html'</script>";
+          }else{
+            echo"<script language='javascript' type='text/javascript'>
+            alert('Não foi possível cadastrar esse usuário');window.location
+            .href='cadastro.html'</script>";
+          }
         }
-      }
-    }
-?>
+      }*/
+  ?>
+</html>
