f5f871b9d7d3edd57e4cc0f5c79cd8ffdb49cf92
JV-Repo <joao.assumpo@gmail.com>
2022-05-04 17:50:53 -0300

    BackEnd do Cadastro Funcionando

--- Modelo-SQL/DB-EcoStore.sql
+++ Modelo-SQL/DB-EcoStore.sql
@@ -9,18 +9,19 @@ create table Usuario(
     DataNasc date,
     Senha varchar(100),
     CPF int,
-    Endereco int,
-    Telefone int,
     TipoUsuario varchar(10),
     primary key (CodUsu)
-);
+)
+ENGINE = InnoDB;
+
 
 create table Telefone(
 	CodTel int NOT NULL AUTO_INCREMENT,
     CodUsu int NOT NULL,
     Numtel varchar(20),
     primary key (CodTel)
-);
+)
+ENGINE = InnoDB;
 
 create table Endereco(
 	CodEnd int NOT NULL AUTO_INCREMENT,
@@ -30,7 +31,8 @@ create table Endereco(
     Numero int,
     Complemento varchar(25),
 	primary key (CodEnd)
-);
+)
+ENGINE = InnoDB;
 
 create table Vendedor(
 	CodVend int NOT NULL AUTO_INCREMENT,
@@ -38,7 +40,8 @@ create table Vendedor(
     NomeEmp varchar(50),
     Descricao varchar(255),
     primary key (CodVend)
-);
+)
+ENGINE = InnoDB;
 
 create table Produto(
 	CodProduto int NOT NULL AUTO_INCREMENT,
@@ -48,7 +51,8 @@ create table Produto(
     ValorUni double,
     Foto mediumblob,
     primary key (CodProduto)
-);
+)
+ENGINE = InnoDB;
 
 create table Venda(
 	CodVenda int NOT NULL AUTO_INCREMENT,
@@ -58,11 +62,8 @@ create table Venda(
     Quantidade int,
     DataPag date,
     primary key (CodVenda)
-);
-
-alter table Usuario
-add foreign key (Endereco) references Endereco(CodEnd),
-add foreign key (Telefone) references Telefone(CodTel);
+)
+ENGINE = InnoDB;
 
 alter table Telefone
 add foreign key (CodUsu) references Usuario(CodUsu);
