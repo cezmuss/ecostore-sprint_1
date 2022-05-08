e1b67d2dfae3f88fb3b7035c3577ca0d4ffeff6d
joaoparrilha <72392593+joaoparrilha@users.noreply.github.com>
2022-05-08 12:32:15 -0300

    Add files via upload

---
+++ usuCad.php
@@ -0,0 +1,32 @@
+<html>
+
+    <head>
+        <title> Cadastro de usuário </title>
+        <link href="css/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale+1.0">
+    </head>
+
+    <body>
+        <div class="logo"><img src="images/logoo.png" width="100px"   />
+            <h2> Ecostore </h2>
+        </div>
+            <div class="menu" >
+                <a href="#">Home</a>
+                <a href="#">Categorias</a>
+                <a href="#">Ofertas</a>
+                <a href="#">Cadastro</a>
+                <a href="usuCad.php">Usuários Cadastrados</a>
+            </div>
+                <h1> Usuários Cadastrados</h1>
+                <?php               
+
+                ?>
+                 < a  href ='' > < img  src ='images/edit.png' title =' Editar Professor ' width =' 32 ' > </ a >
+                </ td > < td >
+                < a  href ='' > < img  src ='images/excluir.png' title =' Excluir Professor ' width =' 32 ' > </ a >
+                </ td >
+                </ tr >
+
+    </body>
+    
+</html>
