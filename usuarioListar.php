968dd9dd351c128f303b26d93136eb6b52df9b3c
JV-Repo <joao.assumpo@gmail.com>
2022-05-08 15:03:21 -0300

    Draf

--- usuarioListar.php
+++ usuarioListar.php
@@ -1,13 +1,41 @@
-<?php
+html>
 
-    /* Conexão usando OOP do PHP */
-    $conn = new mysqli("localhost", "root", "", "EcoStore");
+<head>
+    <title> Cadastro de usuário </title>
+    <link href="css/style.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale+1.0">
+</head>
 
-    /* Checa conexão */
-    if ($conn->connect_error) {
-        die("Conexão Falhou: " . $conn->connect_error);
-    }
+<body>
+    <div class="logo"><img src="images/logoo.png" width="100px" />
+        <h2> Ecostore </h2>
+    </div>
+    <div class="menu">
+        <a href="#">Home</a>
+        <a href="#">Categorias</a>
+        <a href="#">Ofertas</a>
+        <a href="#">Cadastro</a>
+        <a href="usuCad.php">Usuários Cadastrados</a>
+    </div>
+    <h1> Usuários Cadastrados</h1>
+    <?php
+        /* Conexão usando OOP do PHP */
+        $conn = new mysqli("localhost", "root", "", "EcoStore");
 
-    
+        /* Checa conexão */
+        if ($conn->connect_error) {
+            die("Conexão Falhou: " . $conn->connect_error);
+        }
 
-?>
+        $sqlQuery = "";
+
+        echo "
+            <table> 
+
+        ";
+
+        $conn->close();
+    ?>
+</body>
+
+</html>
