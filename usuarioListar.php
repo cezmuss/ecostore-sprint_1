03125d3fb4d1021e9634160049786827632758e3
JV-Repo <joao.assumpo@gmail.com>
2022-05-08 19:02:33 -0300

    Tabela usuarios funcionando

--- usuarioListar.php
+++ usuarioListar.php
@@ -1,35 +1,62 @@
 <html>
 
 <head>
-    <title> Cadastro de usuário </title>
-    <link href="css/style.css" rel="stylesheet" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale+1.0">
+	<title> Cadastro de usuário </title>
+	<link href="css/style.css" rel="stylesheet" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale+1.0">
 </head>
 
 <body>
-    <?php require 'menu.php'; ?>
-    <?php
-        /* Conexão usando OOP do PHP */
-        $conn = new mysqli("localhost", "root", "", "EcoStore");
+	<?php require 'menu.php'; ?>
+	<?php
+	/* Conexão usando OOP do PHP */
+	$conn = new mysqli("localhost", "root", "", "EcoStore");
 
-        /* Checa conexão */
-        if ($conn->connect_error) {
-            die("Conexão Falhou: " . $conn->connect_error);
-        }
+	/* Checa conexão */
+	if ($conn->connect_error) {
+		die("Conexão Falhou: " . $conn->connect_error);
+	}
 
-        $sqlQuery = "SELECT Usuario.CodUsu, Usuario.LoginS, Usuario.Nome, Usuario.CPF, Telefone.NumTel
-                    ";
-        $result = $conn->query($sqlQuery);
+	$sqlQuery = 
+		"SELECT Usuario.CodUsu, Usuario.LoginS, Usuario.Nome, Usuario.CPF, Telefone.NumTel
+		FROM Usuario INNER JOIN Telefone
+		ON Usuario.CodUsu = Telefone.CodUsu;"
+	;
 
-        if ($result->num_rows > 0) {
-            echo "
-                <table> 
+	if ($result = $conn->query($sqlQuery)) {
+		echo "
+			<table>
+					<tr>
+						<th>ID</th>
+						<th>Login</th>
+						<th>Nome</th>
+						<th>CPF</th>
+						<th>Telefone</th>
+					<tr>
+			";
+		if ($result->num_rows > 0){
+			while ($row = $result->fetch_assoc()){
+				$field1name = $row["CodUsu"];
+				$field2name = $row["LoginS"];
+				$field3name = $row["Nome"];
+				$field4name = $row["CPF"];
+				$field5name = $row["NumTel"];
 
-            ";
-        }
+				echo '<tr> 
+							<td>'.$field1name.'</td> 
+							<td>'.$field2name.'</td> 
+							<td>'.$field3name.'</td> 
+							<td>'.$field4name.'</td> 
+							<td>'.$field5name.'</td> 
+						</tr>'
+					;
+			}
+		}
+		echo "</table>";
+	}
 
-        $conn->close();
-    ?>
+	$conn->close();
+	?>
 </body>
 
 </html>
